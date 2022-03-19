39ee96644483225b076bdca038a33cbc5111ce6c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-19 02:34:03 +0000

    Organise event functions

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const path = require('node:path')
-const { writeFile } = require('node:fs/promises')
-const rollup = require('rollup')
-const commonJs = require('@rollup/plugin-commonjs')
-const { nodeResolve } = require('@rollup/plugin-node-resolve')
-const sass = require('sass')
-
 module.exports = function (eleventyConfig, options = {}) {
   // Libraries
   eleventyConfig.setLibrary('md', require('./lib/markdown-it.js'))
@@ -37,7 +30,7 @@ module.exports = function (eleventyConfig, options = {}) {
   eleventyConfig.addGlobalData('config', require('./lib/data/config.js')(options))
   eleventyConfig.addGlobalData('eleventyComputed', require('./lib/data/eleventy-computed.js'))
 
-  // Pass through
+  // Passthrough
   eleventyConfig.addPassthroughCopy({
     'node_modules/govuk-frontend/govuk/assets': 'assets'
   })
@@ -48,41 +41,6 @@ module.exports = function (eleventyConfig, options = {}) {
 
   // Events
   eleventyConfig.on('eleventy.after', async () => {
-    // Default value for dir.output is not provided by eleventyConfig
-    const outputDir = eleventyConfig.dir.output || '_site'
-
-    // Generate CSS
-    try {
-      const cssFile = `${outputDir}/assets/govuk.css`
-      const result = sass.compile(path.join(__dirname, './app/all.scss'), {
-        loadPaths: [
-          __dirname,
-          './node_modules'
-        ],
-        quietDeps: true
-      })
-      writeFile(cssFile, result.css)
-    } catch (error) {
-      console.error(error)
-    }
-
-    // Bundle JavaScript
-    try {
-      const jsFile = `${outputDir}/assets/govuk.js`
-      const bundle = await rollup.rollup({
-        input: path.join(__dirname, './app/all.js'),
-        context: 'window',
-        plugins: [
-          nodeResolve(),
-          commonJs()
-        ]
-      })
-
-      const { output } = await bundle.generate({ format: 'iife' })
-      const { code } = output[0]
-      writeFile(jsFile, code)
-    } catch (error) {
-      console.error(error)
-    }
+    require('./lib/events/generate-govuk-assets.js')(eleventyConfig)
   })
 }
