b196093183848a840a123c82dae6327cb0ae589f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-20 23:48:44 +0000

    Link directly to icon assets

--- .eleventy.js
+++ .eleventy.js
@@ -11,9 +11,9 @@ module.exports = function (eleventyConfig) {
     brandColour: '#28a',
     fontFamily: 'system-ui, sans-serif',
     icons: {
-      mask: '/assets/images/x-govuk-mask-icon.svg',
-      shortcut: '/assets/images/x-govuk-favicon.ico',
-      touch: '/assets/images/x-govuk-apple-touch-icon.png'
+      mask: 'https://raw.githubusercontent.com/x-govuk/logo/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-mask-icon.svg?raw=true',
+      shortcut: 'https://github.com/x-govuk/logo/blob/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-favicon.ico?raw=true',
+      touch: 'https://github.com/x-govuk/logo/blob/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-apple-touch-icon.png?raw=true'
     },
     homeKey: 'GOV.UK Eleventy Plugin',
     parentSite: {
