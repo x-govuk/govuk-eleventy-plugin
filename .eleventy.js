c70ceea969393ccba766f980cd96932730aebdfe
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-25 20:27:58 +0000

    Link icons hosted raw.githubusercontent.com

--- .eleventy.js
+++ .eleventy.js
@@ -11,9 +11,9 @@ module.exports = function (eleventyConfig) {
     brandColour: '#28a',
     fontFamily: 'system-ui, sans-serif',
     icons: {
-      mask: 'https://raw.githubusercontent.com/x-govuk/logo/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-mask-icon.svg?raw=true',
-      shortcut: 'https://github.com/x-govuk/logo/blob/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-favicon.ico?raw=true',
-      touch: 'https://github.com/x-govuk/logo/blob/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-apple-touch-icon.png?raw=true'
+      mask: 'https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-mask-icon.svg?raw=true',
+      shortcut: 'https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-favicon.ico',
+      touch: 'https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-apple-touch-icon.png'
     },
     homeKey: 'GOV.UK Eleventy Plugin',
     parentSite: {
