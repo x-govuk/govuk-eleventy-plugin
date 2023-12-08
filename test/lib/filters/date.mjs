e75988781640da9801f8a290d4cacdc4c0e6df63
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-12-08 17:37:21 +0000

    Complete test coverage for date filter

--- test/lib/filters/date.mjs
+++ test/lib/filters/date.mjs
@@ -9,6 +9,12 @@ describe('Date filter', () => {
     assert.equal(result, '27 December 2021')
   })
 
+  it('Formats a date to ISO 8601 by default', () => {
+    const result = date(new Date('2021-12-27'))
+
+    assert.equal(result, '2021-12-27T00:00:00.000Z')
+  })
+
   it('Formats the date right now', () => {
     const now = Math.round(Date.now() / 1000)
     const result = Math.round(date('now', 'x') / 1000)
