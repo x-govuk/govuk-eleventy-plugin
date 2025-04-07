3214b678ac70bb0c5dc7a07ab4188f7f34c9d330
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-04-07 17:48:25 +0100

    Add tests for virtual template utilities

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -3,7 +3,33 @@ import { describe, it } from 'node:test'
 import fs from 'node:fs/promises'
 import path from 'node:path'
 
-import { getScssSettings, normalise } from '../../lib/utils.js'
+import {
+  getFileContents,
+  getScssSettings,
+  getTemplates,
+  normalise
+} from '../../lib/utils.js'
+
+describe('getFileContents utility', () => {
+  it('Gets file contents', async (t) => {
+    const testContent = 'Test file content'
+    t.mock.method(fs, 'readFile', async () => testContent)
+
+    const result = await getFileContents('/file/path.txt')
+    assert.strictEqual(result, testContent)
+  })
+
+  it('Throws error getting content for non-existent file', async () => {
+    await assert.rejects(
+      () => getFileContents('non-existent-file.txt'),
+      (error) => {
+        assert.ok(error instanceof Error)
+        assert.ok(error.message.includes('ENOENT'))
+        return true
+      }
+    )
+  })
+})
 
 describe('getScssSettings utility', async (t) => {
   const mockDir = { input: '/test' }
@@ -62,6 +88,53 @@ describe('getScssSettings utility', async (t) => {
     assert.equal(result, undefined)
     assert.deepEqual(console.error.mock.calls[0].arguments, [
       `Could not find SCSS settings at ${expectedPath}`
+    ])
+  })
+})
+
+describe('getTemplates utility', () => {
+  const eleventyConfig = { dir: { input: 'src', layouts: 'layouts' } }
+
+  it('Gets all virtual templates', async () => {
+    const result = await getTemplates(eleventyConfig)
+
+    assert.deepEqual(Object.keys(result), [
+      'layouts/base.njk',
+      'layouts/collection.njk',
+      'layouts/feed.njk',
+      'layouts/page.njk',
+      'layouts/post.njk',
+      'layouts/product.njk',
+      'layouts/search-index.njk',
+      'layouts/sitemap.njk',
+      'layouts/sub-navigation.njk',
+      'layouts/tag.njk',
+      'layouts/tags.njk'
+    ])
+  })
+
+  it('Ignores virtual template if layout exists', async (t) => {
+    t.mock.method(fs, 'stat', (filePath) => {
+      if (filePath === 'src/layouts/page.njk') {
+        return Promise.resolve()
+      } else {
+        throw new Error('ENOENT: no such file or directory')
+      }
+    })
+
+    const result = await getTemplates(eleventyConfig)
+
+    assert.deepEqual(Object.keys(result), [
+      'layouts/base.njk',
+      'layouts/collection.njk',
+      'layouts/feed.njk',
+      'layouts/post.njk',
+      'layouts/product.njk',
+      'layouts/search-index.njk',
+      'layouts/sitemap.njk',
+      'layouts/sub-navigation.njk',
+      'layouts/tag.njk',
+      'layouts/tags.njk'
     ])
   })
 })
