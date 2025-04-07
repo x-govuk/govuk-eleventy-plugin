6bb2f78b2c366026c8b32c47e7b6ced76d3569d5
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-04-07 22:04:27 +0100

    Add tests for getScssSettings utility

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -1,9 +1,72 @@
 const assert = require('node:assert/strict')
+const fs = require('node:fs/promises')
+const path = require('node:path')
 const { describe, it } = require('node:test')
 
-const { normalise } = require('../../lib/utils.js')
+const { getScssSettings, normalise } = require('../../lib/utils.js')
 
-describe('Utility', () => {
+describe('getScssSettings utility', async (t) => {
+  const mockDir = { input: '/test' }
+  const defaultContent = '// Default settings'
+  const customPath = 'custom/settings.scss'
+  const customContent = '// Custom settings'
+
+  it('Returns SCSS settings file found at default path', async (t) => {
+    const expectedPath = path.join(mockDir.input, 'sass', '_settings.scss')
+
+    t.mock.method(fs, 'access', (filePath, mode) => {
+      assert.strictEqual(filePath, expectedPath)
+      assert.strictEqual(mode, fs.constants.R_OK)
+      return Promise.resolve()
+    })
+
+    t.mock.method(fs, 'readFile', (filePath, options) => {
+      assert.strictEqual(filePath, expectedPath)
+      assert.deepStrictEqual(options, { encoding: 'utf8' })
+      return Promise.resolve(defaultContent)
+    })
+
+    const result = await getScssSettings(mockDir)
+    assert.strictEqual(result, defaultContent)
+  })
+
+  it('Returns SCSS settings file found at custom path', async (t) => {
+    t.mock.method(fs, 'access', () => Promise.resolve())
+    t.mock.method(fs, 'readFile', async () => customContent)
+
+    const result = await getScssSettings(mockDir, customPath)
+    assert.strictEqual(result, customContent)
+  })
+
+  it('Returns undefined if file at default path missing', async (t) => {
+    t.mock.method(fs, 'access', async () => {
+      throw new Error('File not found')
+    })
+
+    const result = await getScssSettings(mockDir)
+    assert.strictEqual(result, undefined)
+  })
+
+  it('Logs error if file at custom path missing', async (t) => {
+    const expectedPath = `${mockDir.input}/404.scss`
+    const fsError = new Error('File not found')
+    fsError.path = expectedPath
+
+    t.mock.method(fs, 'access', async () => {
+      throw fsError
+    })
+    t.mock.method(console, 'error', () => {})
+
+    const result = await getScssSettings(mockDir, '404.scss')
+
+    assert.equal(result, undefined)
+    assert.deepEqual(console.error.mock.calls[0].arguments, [
+      `Could not find SCSS settings at ${expectedPath}`
+    ])
+  })
+})
+
+describe('normalise utility', () => {
   it('Normalises value provided to a filter', () => {
     const usesValue = normalise('Dollars', 'Pounds')
     const usesDefault = normalise(undefined, 'Pounds')
