1c2c5d67701b27e1ac3011516defb6cdebcd0ac3
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-05-09 23:11:16 +0100

    Replace scssSettingsPath option with native Sass method for passing settings to libraries

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -1,14 +1,8 @@
 import assert from 'node:assert/strict'
 import fs from 'node:fs/promises'
-import path from 'node:path'
 import { describe, it } from 'node:test'
 
-import {
-  getFileContents,
-  getScssSettings,
-  getTemplates,
-  normalise
-} from '../../lib/utils.js'
+import { getFileContents, getTemplates, normalise } from '../../lib/utils.js'
 
 describe('getFileContents utility', () => {
   it('Gets file contents', async (t) => {
@@ -28,67 +22,6 @@ describe('getFileContents utility', () => {
         return true
       }
     )
-  })
-})
-
-describe('getScssSettings utility', async (t) => {
-  const mockDir = { input: '/test' }
-  const defaultContent = '// Default settings'
-  const customPath = 'custom/settings.scss'
-  const customContent = '// Custom settings'
-
-  it('Returns SCSS settings file found at default path', async (t) => {
-    const expectedPath = path.join(mockDir.input, 'sass', '_settings.scss')
-
-    t.mock.method(fs, 'access', (filePath, mode) => {
-      assert.strictEqual(filePath, expectedPath)
-      assert.strictEqual(mode, fs.constants.R_OK)
-      return Promise.resolve()
-    })
-
-    t.mock.method(fs, 'readFile', (filePath, options) => {
-      assert.strictEqual(filePath, expectedPath)
-      assert.deepStrictEqual(options, { encoding: 'utf8' })
-      return Promise.resolve(defaultContent)
-    })
-
-    const result = await getScssSettings(mockDir)
-    assert.strictEqual(result, defaultContent)
-  })
-
-  it('Returns SCSS settings file found at custom path', async (t) => {
-    t.mock.method(fs, 'access', () => Promise.resolve())
-    t.mock.method(fs, 'readFile', async () => customContent)
-
-    const result = await getScssSettings(mockDir, customPath)
-    assert.strictEqual(result, customContent)
-  })
-
-  it('Returns undefined if file at default path missing', async (t) => {
-    t.mock.method(fs, 'access', async () => {
-      throw new Error('File not found')
-    })
-
-    const result = await getScssSettings(mockDir)
-    assert.strictEqual(result, undefined)
-  })
-
-  it('Logs error if file at custom path missing', async (t) => {
-    const expectedPath = `${mockDir.input}/404.scss`
-    const fsError = new Error('File not found')
-    fsError.path = expectedPath
-
-    t.mock.method(fs, 'access', async () => {
-      throw fsError
-    })
-    t.mock.method(console, 'error', () => {})
-
-    const result = await getScssSettings(mockDir, '404.scss')
-
-    assert.equal(result, undefined)
-    assert.deepEqual(console.error.mock.calls[0].arguments, [
-      `Could not find SCSS settings at ${expectedPath}`
-    ])
   })
 })
 
